ffb7f88677b323185012a5b2f5c94bea0789f3c8
B NAGA VARDHANA <vardhanabobburi35@outlook.com>
2026-02-07 23:15:16 +0530

    Updated FastAPI to serve frontend and fixed HTML static paths

--- backend/main.py
+++ backend/main.py
@@ -8,9 +8,6 @@ from backend import models, schemas
 from backend.auth import router as auth_router, get_current_user
 
 from fastapi.middleware.cors import CORSMiddleware
-from fastapi.staticfiles import StaticFiles
-from fastapi.responses import FileResponse
-import os
 
 
 
@@ -30,14 +27,10 @@ app.add_middleware(
 
 app.include_router(auth_router)
 
-# Serve static files (CSS, JS)
-app.mount("/static", StaticFiles(directory="frontend"), name="static")
 
-# Serve frontend homepage
 @app.get("/")
-def serve_frontend():
-    return FileResponse(os.path.join("frontend", "index.html"))
-
+def read_root():
+    return {"message": "Employee Task Management API is running"}
 
 
 # ------------------------------------------------------------
